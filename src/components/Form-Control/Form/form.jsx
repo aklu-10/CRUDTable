0fa159a784ff727f7e66f63e04837cb73ae527ca
akaSh_T <96860174+aklu-10@users.noreply.github.com>
2023-06-30 00:41:43 +0530

    CRUD

--- src/components/Form-Control/Form/form.jsx
+++ src/components/Form-Control/Form/form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import editApi from '../../Api/fetchApi';
 
-const Form = ({ data, handleSubmit, isValue = true,formData,setFormData  }) => {
+const Form = ({ handleSubmit, formData,setFormData, isAddForm=false  }) => {
 
 const[show,setShow]=useState(true)
 
@@ -25,38 +25,21 @@ const[show,setShow]=useState(true)
     })
   }
   
-  function emptyData(obj)
-{
-    let resultObj= {}    
-    function emptyNestedData(obj)
-    {
-        Object.keys(obj).map(element=>
-        {
-            if(typeof obj[element] === 'object')
-                emptyNestedData(obj[element])
-            else
-                obj[element]=''; 
-        })
-        resultObj = obj;
-    }
-    emptyNestedData(obj)
-    return resultObj;
-}
-//setFormData(emptyData(data));
-  return (
-   
-    show?<div  className='position-absolute top-0 start-50 bg-dark fs-5 text-light p-2 rounded w-50 text-start'>
-    <h1 className='text-center border-bottom border-success'>Update Form</h1> 
+ 
+return (
+  
+  show?<div  className='position-absolute top-0 start-50 bg-dark fs-5 text-light p-2 rounded w-50 text-start'>
+    <h1 className='text-center border-bottom border-success'> {isAddForm ? 'Add Form ' : 'Update Form' } </h1> 
 	<form onSubmit={handleSubmit}>
     {
       Object.keys(formData).map(element => {
         if (typeof formData[element] === 'object')
           return <><h5>{element}</h5>{handleInputs(formData[element], element)}</>
         else
-          return <><label>{element}</label><input type="text" value={formData[element]} onChange={(e) => setFormData({ ...formData, [element]: e.target.value })} className='form-control' /></>
+          return <><label>{element}</label><input type="text" value={formData[element]} onChange={(e)=>setFormData({...formData, [element] : e.target.value})} className='form-control' /></>
       })
     }
-    <button type='submit' className='btn btn-outline-warning w-100 mt-2 p-2' onClick={()=>setShow(false)}>Update</button>
+    <button type='submit' className='btn btn-outline-warning w-100 mt-2 p-2' >{isAddForm ? 'Add' : 'Update' }</button>
   </form></div>:null
    
     
